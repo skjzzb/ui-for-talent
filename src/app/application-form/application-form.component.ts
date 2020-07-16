f4247d44e1a225e68efeb5420568541120bcbaee
Saurabh Jadhav <saurabhjk07@gmail.com>
2020-07-16 22:58:06 +0530

    application form

---
+++ src/app/application-form/application-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ngx-application-form',
+  templateUrl: './application-form.component.html',
+  styleUrls: ['./application-form.component.scss']
+})
+export class ApplicationFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
