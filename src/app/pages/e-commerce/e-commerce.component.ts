f66f5293ac63b36e182e696d68ff00673ebf5cd6
Saurabh Jadhav <GSC-30537@GSLAB.COM>
2020-08-18 09:47:12 +0530

    Extra

--- src/app/pages/e-commerce/e-commerce.component.ts
+++ src/app/pages/e-commerce/e-commerce.component.ts
@@ -31,23 +31,7 @@ ngOnInit(): void {
   this.logindetails =  JSON.parse(sessionStorage.getItem('user_info'))
   console.log(this.logindetails.roles)
   this.currentRole=this.logindetails.roles[0];
-    // if(this.logindetails.roles[0]=='ROLE_USER')
-    // {
-    //   this.currentRole='ROLE_USER'
-
-    // }else if(this.logindetails.roles[0]=='ROLE_PANEL')
-    // {
-    //   this.currentRole='ROLE_PANEL'
-
-    // }else if(this.logindetails.roles[0]=='ROLE_HR')
-    // {
-    //   this.currentRole='ROLE_HR'
-
-    // }else if(this.logindetails.roles[0]=='ROLE_ADMIN')
-    // {
-    //   this.currentRole='ROLE_ADMIN'
-
-    // }
+    
     this.service.getAllConfirmedScheduledInterview().subscribe(data =>{
       this.interviewData = data
       this.interviewData.forEach(element => {
