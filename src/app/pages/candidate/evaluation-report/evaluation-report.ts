16b5dc5084628362dfeaac5dea0d26673a0fec73
Ashwini V Thorave <GS-2484@GSLAB.COM>
2020-08-24 12:25:10 +0530

    evaluation component fix

--- src/app/pages/candidate/evaluation-report/evaluation-report.ts
+++ src/app/pages/candidate/evaluation-report/evaluation-report.ts
@@ -1,28 +1,9 @@
-import { Component, OnInit, EventEmitter, Output, Input } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, Input, ElementRef, ContentChildren } from '@angular/core';
 import { DataService } from '../../../@core/utils/data.service';
-import { TokenStorageService } from '../../../_services/token-storage.service';
 import { Router, ActivatedRoute } from '@angular/router';
-import { NbSortDirection, NbSortRequest, NbTreeGridDataSource, NbTreeGridDataSourceBuilder } from '@nebular/theme';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
-interface TreeNode<T> {
-  data: T;
-  children?: TreeNode<T>[];
-  expanded?: boolean;
-}
-
-// interface FSEntry {
-//   name: string;
-//   size: string;
-//   kind: string;
-//   items?: number;
-// }
-
-interface FSEntry {
-  name: string;
-  size: string;
-  kind: string;
-  items?: number;
-}
 
 @Component({
   selector: 'ngx-evaluation-report',
@@ -33,37 +14,19 @@ export class EvaluationReportComponent implements OnInit {
   candidate: any;
   vacancy : any;
   technologies : any[];
-  concepts: any[];
+  concepts: Observable<any>;
   questions: any;
-  ratings:any;
-  flag=false;
-
-  customColumn = 'name';
-  defaultColumns = [ 'size', 'kind', 'items' ];
-  allColumns = [ this.customColumn, ...this.defaultColumns ];
-
-  dataSource: NbTreeGridDataSource<FSEntry>;
-
-  sortColumn: string;
-  sortDirection: NbSortDirection = NbSortDirection.NONE;
+  groups:any;
 
   constructor(private service:DataService,
-              private dataSourceBuilder: NbTreeGridDataSourceBuilder<FSEntry>,
               private router: Router,
-              private route :ActivatedRoute){
-
-                this.ratings= [];
-                this.flag=false;
-                this.dataSource = this.dataSourceBuilder.create(this.data);
+              private route :ActivatedRoute,
+              private elementRef:ElementRef){
               }
 
-
   ngOnInit(): void {
-   // this.example = this.route.snapshot.params['rowData'];
-   // console.log(this.route.snapshot.params['rowData'])
     this.candidate = JSON.parse(this.route.snapshot.params["rowData"]);
     this.vacancy = this.candidate.vacancy;
-    console.log(this.candidate.email)
     console.log(this.candidate);
 
     var list = this.vacancy.jd.replace(/\s/g, '').split(",");
@@ -73,97 +36,91 @@ export class EvaluationReportComponent implements OnInit {
      {
        console.log(result.data)
        this.technologies = result.data;
-       for( var i = 0 ; i<this.technologies.length;i++)
-       {
-         this.service.getConceptById(this.technologies[i]).then
-         (result=>
-           {
-             console.log(result);
-             this.concepts = result.data;
-            this.questions =  this.concepts.map(element=>
-              {
-                console.log( element.question.split(","));
-                return {"concept":element.concept,"question" :element.question.split(",")};
-              })
-              console.log(this.questions);
-           })
-        }
-     })
+       //  for( var i = 0 ; i<this.technologies.length;i++)
+      //  {
+      //    this.service.getConceptById(this.technologies[i]).then
+      //    (result=>
+      //      {
+      //        console.log(result);
+      //        this.concepts = result.data;
+      //       this.questions =  this.concepts.map(element=>
+      //         {
+      //           console.log( element.question.split(","));
+      //           return {"concept":element.concept,"question" :element.question.split(",")};
+      //         })
+      //         console.log(this.questions);
+      //      })
+      //   }
+    })
   }
 
+  getObject($event:any)
+  {
+    this.questions = $event;
+  }
   onSubmit(UIform)
   {
     console.log(UIform.form.value);
-    console.log(this.ratings);
+    // console.log(this.ratings);
   }
-clickme()
-{
-  this.flag = true;
-}
 
-
-updateSort(sortRequest: NbSortRequest): void {
-  this.sortColumn = sortRequest.column;
-  this.sortDirection = sortRequest.direction;
-}
-
-getSortDirection(column: string): NbSortDirection {
-  if (this.sortColumn === column) {
-    return this.sortDirection;
-  }
-  return NbSortDirection.NONE;
-}
-
-private data: TreeNode<FSEntry>[] = [
-  {
-    data: { name: 'Projects', size: '1.8 MB', items: 5, kind: 'dir' },
-    children: [
-      { data: { name: 'project-1.doc', kind: 'doc', size: '240 KB' } },
-      { data: { name: 'project-2.doc', kind: 'doc', size: '290 KB' } },
-      { data: { name: 'project-3', kind: 'txt', size: '466 KB' } },
-      { data: { name: 'project-4.docx', kind: 'docx', size: '900 KB' } },
-    ],
-  },
-  {
-    data: { name: 'Reports', kind: 'dir', size: '400 KB', items: 2 },
-    children: [
-      { data: { name: 'Report 1', kind: 'doc', size: '100 KB' } },
-      { data: { name: 'Report 2', kind: 'doc', size: '300 KB' } },
-    ],
-  },
-  {
-    data: { name: 'Other', kind: 'dir', size: '109 MB', items: 2 },
-    children: [
-      { data: { name: 'backup.bkp', kind: 'bkp', size: '107 MB' } },
-      { data: { name: 'secret-note.txt', kind: 'txt', size: '2 MB' } },
-    ],
-  },
-];
-
-getShowOn(index: number) {
-  const minWithForMultipleColumns = 400;
-  const nextColumnStep = 100;
-  return minWithForMultipleColumns + (nextColumnStep * index);
-}
 
 }
 
 @Component({
-  selector: 'ngx-fs-icon',
+  selector: 'panel',
   template: `
-    <nb-tree-grid-row-toggle [expanded]="expanded" *ngIf="isDir(); else fileIcon">
-    </nb-tree-grid-row-toggle>
-    <ng-template #fileIcon>
-      <nb-icon icon="file-text-outline"></nb-icon>
-    </ng-template>
-  `,
-})
-export class FsIconComponent {
-  @Input() kind: string;
-  @Input() expanded: boolean;
+  <div class="card card-info">
+    <div class="card-header" (click)="toggle(title)">
+      {{title}}
+    </div>
+      <div class="card-body" *ngIf="opened">
+      <div class="row" *ngFor="let q of questions"  >
+                <label class="label">{{q.concept}}</label>
 
-  isDir(): boolean {
-    return this.kind === 'dir';
+        <div class="col-md-4" *ngFor="let que of q.question" >
+                <label class="label">{{que}}</label>
+              <input type="number"
+                    [ngModel]="ratings[que]"
+                     nbInput
+                     name="que"
+                    >
+                  </div>
+          </div>
+    </div>
+  <div>
+  `
+})
+export class PanelComponent {
+  @Input() opened = false;
+  @Input() title: string;
+  // @Output() toggle: EventEmitter<any> = new EventEmitter<any>();
+  @Output() getObject = new EventEmitter<any>();
+
+  concepts:any;
+  questions:any;
+  ratings:any;
+  constructor(private service:DataService){
+    this.ratings=[];
+    }
+
+  toggle(title)
+  {
+    this.opened = !this.opened;
+    this.service.getConceptById(title).subscribe
+    (result=>
+      {
+        console.log(result);
+        this.concepts = result;
+       this.questions =  this.concepts.map(element=>
+         {
+           console.log( element.question.split(","));
+          return {"concept":element.concept,"question" :element.question.split(",")};
+         })
+         console.log(this.questions);
+         this.getObject.emit(this.questions);
+      })
+    console.log(title)
   }
 }
 
