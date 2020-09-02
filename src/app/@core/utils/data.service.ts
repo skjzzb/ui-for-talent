0b4c1bbb7613409b4fc7f830fd898fa4b5976646
Ishwari T Pednekar <GS-2483@GSLAB.COM>
2020-09-02 11:47:20 +0530

    fb page working

--- src/app/@core/utils/data.service.ts
+++ src/app/@core/utils/data.service.ts
@@ -241,4 +241,8 @@ export class DataService {
         //return this.http.post("https://localhost:3000/post-fb",vacancy);
       return this.http.post("https://social-fb-app.herokuapp.com/post-fb",vacancy);
   }
+
+  getCandidateByProjectName(projName){
+    return this.http.get(`https://cv-processing-api.herokuapp.com/v1/getCandidateByProject/${projName}`);
+  }
 }
